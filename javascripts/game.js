7cb22ce5f2de92bd07681d99c7a9ed8d9668979c
Shane Riley <shane@shaneriley.com>
2015-03-09 12:09:34 -0400

    Add extra credit guess count

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,14 +1,16 @@
 $(function() {
   var answer = Math.floor(Math.random() * 100) + 1,
       $message = $("p"),
-      starting_message = $message.text();
+      starting_message = $message.text(),
+      guesses = 0;
 
   $("form").on("submit", function(e) {
     e.preventDefault();
 
     var guess = +$("#guess").val();
+    guesses++;
     if (guess === answer) {
-      $message.text("You guessed it!");
+      $message.text("You guessed it! It took you " + guesses + " guesses.");
     }
     else if (guess > answer) {
       $message.text("My number is lower than " + guess);
@@ -21,6 +23,7 @@ $(function() {
   $("a").on("click", function(e) {
     e.preventDefault();
     answer = Math.floor(Math.random() * 100) + 1;
+    guesses = 0;
     $message.text(starting_message);
   });
 });
